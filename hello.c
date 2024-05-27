b3342c65219fc9c8293e54adbf91695002376455
tkddn0321 <godsknight58@naver.com>
2024-05-27 18:42:17 +0900

    okokokok

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 100;
+    int b = 200;
+    int c = a+b;
+
+    printf("%d + %d = %d ..\n",a,b,c);
+
+    printf("Hello, World!\n");
+    return 0;
+}
