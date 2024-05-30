c8dd7b029f14f4b22c32fb4704c246338b165bc6
tkddn0321 <godsknight58@naver.com>
2024-05-30 17:47:50 +0900

    0530

---
+++ java-lang/app/src/main/java/Test4.java
@@ -0,0 +1,12 @@
+// 실습
+// - 컴파일하고 실행하라.
+//
+// 결과 및 설명
+// - main() 메서드의 형식이 옳지 않다.
+//      public static void main(String[] a)
+
+class Test4 {
+    public static void main(String[] a) {
+
+    }
+}
---
+++ java-lang/app/src/main/java/Test6.java
@@ -0,0 +1,30 @@
+// 실습
+// - 애플리케이션 아규먼트를 다음과 같이 결과가 나오도록 모두 출력하라.
+//      $ java -classpath ... Test6 aaa bbb ccc ddd
+//      'aaa'
+//      'bbb'
+//      'ccc'
+//      'ddd'
+// 결과 및 설명
+// - 
+// 
+
+class Test6 {
+    public static void main(String[] args) {
+        // 코드를 완성하시오.
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("'" + args[i] + "'");
+        }
+
+        // System.out.println(args[0]);
+        // System.out.println(args[1]);
+        // System.out.println(args[2]);
+        // System.out.println(args[3]);
+
+        // System.out.println("'" + args[0] + "'");
+        // System.out.println("'" + args[1] + "'");
+        // System.out.println("'" + args[2] + "'");
+        // System.out.println("'" + args[3] + "'");
+
+    }
+}
