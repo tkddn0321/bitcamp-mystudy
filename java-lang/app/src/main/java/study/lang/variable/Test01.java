a480a072874dd22e0ec343cff9d242a814d4328e
tkddn0321 <godsknight58@naver.com>
2024-06-04 16:46:20 +0900

    0604

---
+++ java-lang/app/src/main/java/study/lang/variable/Test01.java
@@ -0,0 +1,42 @@
+package study.lang.variable;
+
+public class Test01 {
+  public static void main(String[] args) {
+    // 실습
+    // - primitive type의 변수를 선언하라.
+    // - 각 변수에 최소값, 최대값을 할당하라.
+    // - 각 변수에 최소값, 최대값 범위를 벗어나는 값을 넣은 후 오류를 확인하라.
+    // - 예)
+    // byte b1 = -128;
+    // byte b2 = 127;
+
+    byte b1 = -128; // 최소값
+    byte b2 = 127; // 최대값
+
+    short s1 = -32768; // 최소값
+    short s2 = 32767; // 최대값
+
+    int i1 = -21_4748_3648; // 최소값
+    int i2 = 21_4748_3647; // 최대값
+
+    long l1 = -922_3372_0368_5477_5808L; // 최소값
+    long l2 = 922_3372_0368_5477_5807L; // 최대값
+
+    float f1 = 32.17541f; // 최소값
+    float f2 = 23447.12f; // 최대값
+    // float f3 = f1 + f2;
+
+    double d1 = 987654323456.789; // 최소값 15자리
+    double d2 = 0.0; // 최대값
+
+    // System.out.println(f3);
+
+    char c1 = 0; // 최소값
+    char c2 = 65535; // 최대값
+
+    boolean bool1 = false; // 최소값
+    boolean bool2 = false; // 최대값
+
+
+  }
+}
---
+++ java-lang/app/src/main/java/study/lang/variable/Test02.java
@@ -0,0 +1,19 @@
+package study.lang.variable;
+
+// 실습
+// - 문자 코드를 저장하는 방법을 확인하라.
+public class Test02 {
+  public static void main(String[] args) {
+
+    // 코드를 완성하라.
+    char c1 = 44032; // 변수에 '가' 문자의 코드를 정수 값으로 저장하라.
+    char c2 = 0xAC00;
+    char c3 = '\uAC00'; // 변수에 '가' 문자의 코드를 \u0000 형태의 유니코드 표현법으로 저장하라.
+    char c4 = '가'; // 변수에 '가' 문자의 코드를 가장 쉬운 방법으로 저장하라.
+
+    System.out.println(c1);
+    System.out.println(c2);
+    System.out.println(c3);
+    System.out.println(c4);
+  }
+}
