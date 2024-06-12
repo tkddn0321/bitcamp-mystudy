c336551ff841529407db8b595ce2a3f1cea04d6b
tkddn0321 <godsknight58@naver.com>
2024-06-12 19:53:36 +0900

    0612

---
+++ java-lang/app/src/main/java/study/lang/method/Test07.java
@@ -0,0 +1,26 @@
+package study.lang.method;
+
+public class Test07 {
+  public static void main(String[] args) {
+    // sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    int sum = 0;
+    for(int i = 1; i <= 10; i++) {
+      sum += i;
+    }
+    System.out.println(sum);
+
+    // sum(10) = 10 + sum(9)
+    //         = 10 + 9 + sum(8)
+    //         = 10 + 9 + 8 + sum(7)
+    //         = 10 + 9 + 8 + ... + sum(1)
+    // sum(n)  = n + sum(n-1)
+    System.out.println(sum(10));
+  }
+
+  static int sum(int n) {
+    if (n == 1) {
+      return 1;
+    }
+    return n + sum(n-1);
+  }
+}
