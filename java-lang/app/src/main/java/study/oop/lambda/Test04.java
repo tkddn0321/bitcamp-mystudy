e47741e5ac8b152d179dd184c976b16eec9923a3
tkddn0321 <godsknight58@naver.com>
2024-07-16 17:46:45 +0900

    0716 git commit -m 0716 git add . cd ..

---
+++ java-lang/app/src/main/java/study/oop/lambda/Test04.java
@@ -0,0 +1,54 @@
+package study.oop.lambda;
+
+public class Test04 {
+
+  interface Calculator {
+    int plus(int a, int b);
+  }
+
+  static void test(Calculator c) {
+    System.out.println(c.plus(100, 200));
+  }
+
+  public static void main(String[] args) {
+    // 1) 일반 클래스
+    class Calc implements Calculator {
+      @Override
+      public int plus(int a, int b) {
+        return a + b;
+      }
+    }
+    Calculator c1 = new Calc();
+    test(c1);
+
+    // 2) 익명 클래스
+    Calculator c2 = new Calculator() {
+      @Override
+      public int plus(int a, int b) {
+        return a + b;
+      }
+    };
+    test(c2);
+
+    //3)익명 클래스 직접 대입
+    test(new Calculator() {
+      @Override
+      public int plus(int a, int b) {
+        return a + b;
+      }
+    });
+
+    // 4) 람다
+    Calculator c3 = (int a, int b) -> {
+      return a + b;
+    };
+    test(c3);
+
+    // 5) 람다 + 중괄호 생략
+    Calculator c4 = (int a, int b) -> a + b;
+    test(c4);
+
+    // 6) 람다 직접 대입
+    test((int a, int b) -> a + b);
+  }
+}
--- myapp/app/src/main/java/bitcamp/myapp/command/HelpCommand.java
+++ myapp/app/src/main/java/bitcamp/myapp/command/HelpCommand.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class HelpCommand implements Command {
 
-  public void execute(Stack<String> menuPath) {
+  public void execute(String menuName) {
     System.out.println("도움말입니다!");
   }
 }
